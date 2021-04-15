75b1a609b82423eccc6342ab5cee2b5b03a57c8a
majorxma <majorxma@163.com>
2021-04-15 16:49:09 +0800

    2021.04.15

--- push.sh
+++ push.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-time = $(date)
+hugo
+time4=$(date "+%Y.%m.%d")
+echo $time4
+cd C:\Users\MKXDCCDB\Desktop\mcx797\
 git add --all
-git commit -m $time
-echo "1"
+git commit -m $time4
+git push -u orign site:site
+time5=$(date "+%Y.%m.%d")
+cd C:\Users\MKXDCCDB\Desktop\mcx797\public
+git add --all
+git commit -m $time5
+git push -u origin public:public
