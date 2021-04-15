5cf9931b8d4beeccd3aa1fbbee627e7e854d0d61
majorxma <majorxma@163.com>
2021-04-15 16:49:56 +0800

    2021.04.15

--- push.sh
+++ push.sh
@@ -2,7 +2,7 @@
 hugo
 time4=$(date "+%Y.%m.%d")
 echo $time4
-cd C:\Users\MKXDCCDB\Desktop\mcx797\
+cd C:\Users\MKXDCCDB\Desktop\mcx797
 git add --all
 git commit -m $time4
 git push -u orign site:site
